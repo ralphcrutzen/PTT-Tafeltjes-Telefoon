8f956422bbf305b22800a9343a172c8976f5fc17
Ralph Crützen <ralph@crutzen.eu>
2017-02-24 20:31:27 +0100

    Update t65.py
    * Audio toegevoegd
    * Structuur van de code verbeterd

--- t65.py
+++ t65.py
@@ -1,25 +1,23 @@
 import RPi.GPIO as GPIO
 import time
 import random
+import pygame
 
 GPIO.setmode(GPIO.BCM)
-
 GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-klaar = True
+pygame.mixer.init()
 
-while True:
-    if klaar == True:
-        getal1 = random.randint(1,10)
-        getal2 = random.randint(1,10)
-        uitkomst = getal1 * getal2
-        nCijfers = len(str(uitkomst))
-        print "Wat is", getal1, "x", getal2,"?"
-        huidigCijfer = 0
-        antwoord = ""
 
+def speel(bestand):
+    pygame.mixer.music.load(bestand)
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy() == True:
+        continue
+
+
+def getNummer():
     nPulsen = 0
-
     # Wacht op draaischijf
     schijfContact = GPIO.input(18)
     while schijfContact == False:
@@ -39,17 +37,37 @@ while True:
                 klaar = True
             schijfContact = GPIO.input(18)
 
-        # Controleer uitkomst
-        if klaar == True:
-            nPulsen = nPulsen % 10 # 10 pulsen is het cijfer 0
-            antwoord = antwoord + str(nPulsen)
-            huidigCijfer = huidigCijfer + 1
-            if huidigCijfer == nCijfers:
-                print antwoord
-                if int(antwoord) == uitkomst:
-                    print "Goed zo!"
-                else:
-                    print "Jammer, de juiste uitkomst is", uitkomst
-                print
-            else:
-                klaar = False
+    if klaar == True:
+        return nPulsen % 10
+
+
+while True:
+    # Bepaal opgave
+    getal1 = random.randint(1,10)
+    getal2 = random.randint(1,10)
+    uitkomst = getal1 * getal2
+    nCijfers = len(str(uitkomst))
+    print "Wat is", getal1, "x", getal2,"?"
+    speel("audio/" + str(getal1) + ".mp3")
+    speel("audio/keer.mp3")
+    speel("audio/" + str(getal2) + ".mp3")
+    speel("audio/is.mp3")
+
+    huidigCijfer = 0
+    antwoord = ""
+
+    # Wacht op antwoord
+    while huidigCijfer < nCijfers:
+        nummer = getNummer()
+        antwoord = antwoord + str(nummer)
+        huidigCijfer = huidigCijfer + 1
+    print antwoord
+
+    # Controleer antwoord
+    if int(antwoord) == uitkomst:
+        print "Goed zo!"
+        speel("audio/goed.mp3")
+    else:
+        print "Jammer, de juiste uitkomst is", uitkomst
+        speel("audio/fout.mp3")
+    print
