b4999209a7610556703c129ad362684194007b67
Ralph Crützen <ralph@crutzen.eu>
2017-02-25 21:39:01 +0100

    Aardtoets
    Aardtoets functionaliteit toegevoegd: bij het indrukken van de knop wordt de som herhaald en kan deze opnieuw worden ingevoerd.

--- t65.py
+++ t65.py
@@ -1,10 +1,18 @@
+#!/usr/bin/env python
+
 import RPi.GPIO as GPIO
 import time
 import random
 import pygame
 
+SCHIJFPIN = 18
+AARDPIN = 21
+HOORNPIN = 24
+
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(SCHIJFPIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(HOORNPIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(AARDPIN, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Laag als ingedruk!!!
 
 pygame.mixer.init()
 
@@ -16,12 +24,26 @@ def speel(bestand):
         continue
 
 
+def speelSom(getal1, getal2):
+    print "Wat is", getal1, "x", getal2,"?"
+    speel("audio/" + str(getal1) + ".mp3")
+    speel("audio/keer.mp3")
+    speel("audio/" + str(getal2) + ".mp3")
+    speel("audio/is.mp3")
+
+
 def getNummer():
     nPulsen = 0
-    # Wacht op draaischijf
-    schijfContact = GPIO.input(18)
-    while schijfContact == False:
-        schijfContact = GPIO.input(18)
+    # Wacht op draaischijf en knop
+    schijfContact = GPIO.input(SCHIJFPIN)
+    aardContact = GPIO.input(AARDPIN)
+    while schijfContact == False and aardContact == True:
+        schijfContact = GPIO.input(SCHIJFPIN)
+        aardContact = GPIO.input(AARDPIN)
+
+    # Aardtoets ingedrukt
+    if aardContact == False:
+        return -1
 
     # Afhandelen van pulsen
     klaar = False
@@ -29,16 +51,15 @@ def getNummer():
         nPulsen = nPulsen + 1
         startTijd = time.time()
         time.sleep(0.1)
-        schijfContact = GPIO.input(18)
+        schijfContact = GPIO.input(SCHIJFPIN)
 
         # Controleer tijd tussen twee pulsen
         while klaar == False and schijfContact == False:
             if time.time() - startTijd >= 0.2:
                 klaar = True
-            schijfContact = GPIO.input(18)
+            schijfContact = GPIO.input(SCHIJFPIN)
 
-    if klaar == True:
-        return nPulsen % 10
+    return nPulsen % 10
 
 
 while True:
@@ -47,20 +68,21 @@ while True:
     getal2 = random.randint(1,10)
     uitkomst = getal1 * getal2
     nCijfers = len(str(uitkomst))
-    print "Wat is", getal1, "x", getal2,"?"
-    speel("audio/" + str(getal1) + ".mp3")
-    speel("audio/keer.mp3")
-    speel("audio/" + str(getal2) + ".mp3")
-    speel("audio/is.mp3")
 
+    speelSom(getal1, getal2)
     huidigCijfer = 0
     antwoord = ""
 
     # Wacht op antwoord
     while huidigCijfer < nCijfers:
         nummer = getNummer()
-        antwoord = antwoord + str(nummer)
-        huidigCijfer = huidigCijfer + 1
+        if nummer > -1: # aan schijf gedraaid
+            antwoord = antwoord + str(nummer)
+            huidigCijfer = huidigCijfer + 1
+        else: # aardtoets ingedrukt
+            speelSom(getal1, getal2)
+            huidigCijfer = 0
+            antwoord = ""
     print antwoord
 
     # Controleer antwoord
@@ -71,3 +93,4 @@ while True:
         print "Jammer, de juiste uitkomst is", uitkomst
         speel("audio/fout.mp3")
     print
+    time.sleep(1)
