35eb0ce005c99c8a2e32e7ccd76dcbdd7d33048b
Ralph Crützen <ralph@crutzen.eu>
2017-02-27 09:20:11 +0100

    Hoorn opnemen / neerleggen
    * Bij het starten van het script wordt eerst gewacht totdat de hoorn wordt opgenomen.
    * Bij het neerleggen van de hoorn herstart het script.
    * GPIO aansluitingen hoorn en aardknop gewijzigd

--- t65.py
+++ t65.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 
 import RPi.GPIO as GPIO
+import sys
+import os
 import time
 import random
 import pygame
 
 SCHIJFPIN = 18
-AARDPIN = 21
+AARDPIN = 23
 HOORNPIN = 24
 
 GPIO.setmode(GPIO.BCM)
@@ -34,9 +36,9 @@ def speelSom(getal1, getal2):
 
 def getNummer():
     nPulsen = 0
-    # Wacht op draaischijf en knop
+    # Wacht op draaischijf / knop
     schijfContact = GPIO.input(SCHIJFPIN)
-    aardContact = GPIO.input(AARDPIN)
+    aardContact = GPIO.input(AARDPIN) # Laag als ingedrukt!!!
     while schijfContact == False and aardContact == True:
         schijfContact = GPIO.input(SCHIJFPIN)
         aardContact = GPIO.input(AARDPIN)
@@ -62,35 +64,66 @@ def getNummer():
     return nPulsen % 10
 
 
+def hoornCallback(channel):
+    print "Hoorn!", channel
+    # herstart het hele script
+    GPIO.cleanup()
+    python = sys.executable
+    os.execl(python, python, * sys.argv)
+
+# Gebruik een interrupt voor de hoorn,
+# omdat deze op elk moment kan worden neergelegd
+GPIO.add_event_detect(HOORNPIN, GPIO.BOTH, callback = hoornCallback)
+
 while True:
-    # Bepaal opgave
-    getal1 = random.randint(1,10)
-    getal2 = random.randint(1,10)
-    uitkomst = getal1 * getal2
-    nCijfers = len(str(uitkomst))
+    try:
+        # Wacht op hoorn
+        print "Pak de hoorn op!"
+        hoornContact = GPIO.input(HOORNPIN)
+        while hoornContact == True:
+            hoornContact = GPIO.input(HOORNPIN)
+        time.sleep (2)
 
-    speelSom(getal1, getal2)
-    huidigCijfer = 0
-    antwoord = ""
+        # Welk tafeltje oefenen?
+        print "Welk tafeltje?"
+        speel("audio/welk.mp3")
+        tafeltje = getNummer()
 
-    # Wacht op antwoord
-    while huidigCijfer < nCijfers:
-        nummer = getNummer()
-        if nummer > -1: # aan schijf gedraaid
-            antwoord = antwoord + str(nummer)
-            huidigCijfer = huidigCijfer + 1
-        else: # aardtoets ingedrukt
+        for som in range(10):
+            # Bepaal opgave
+            getal1 = random.randint(1,10)
+            getal2 = tafeltje
+            if tafeltje == -1:
+                getal2 = random.randint(1,10)
+            uitkomst = getal1 * getal2
+            nCijfers = len(str(uitkomst))
+
             speelSom(getal1, getal2)
             huidigCijfer = 0
             antwoord = ""
-    print antwoord
 
-    # Controleer antwoord
-    if int(antwoord) == uitkomst:
-        print "Goed zo!"
-        speel("audio/goed.mp3")
-    else:
-        print "Jammer, de juiste uitkomst is", uitkomst
-        speel("audio/fout.mp3")
-    print
-    time.sleep(1)
+            # Wacht op antwoord
+            while huidigCijfer < nCijfers:
+                nummer = getNummer()
+                if nummer > -1: # aan schijf gedraaid
+                    antwoord = antwoord + str(nummer)
+                    huidigCijfer = huidigCijfer + 1
+                else: # aardtoets ingedrukt
+                    speelSom(getal1, getal2)
+                    huidigCijfer = 0
+                    antwoord = ""
+            print antwoord
+
+            # Controleer antwoord
+            if int(antwoord) == uitkomst:
+                print "Goed zo!"
+                speel("audio/" + str(antwoord) + ".mp3")
+                speel("audio/goed.mp3")
+            else:
+                print "Jammer, de juiste uitkomst is", uitkomst
+                speel("audio/fout.mp3")
+                speel("audio/" + str(uitkomst) + ".mp3")
+            print
+            time.sleep(1)
+    except KeyboardInterrupt: # Ctrl+C
+        GPIO.cleanup()
